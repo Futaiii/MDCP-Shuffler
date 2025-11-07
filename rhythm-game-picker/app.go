ba7d0789e5806aaae4b5e31c147720771c100666
ccbling <blingcc233@gmail.com>
2025-11-07 21:13:28 +0800

    feat. Green ver

--- rhythm-game-picker/app.go
+++ rhythm-game-picker/app.go
@@ -3,11 +3,7 @@ package main
 
 import (
 	"context"
-	_ "embed"
-	"io/ioutil"
 	"math/rand"
-	"os"
-	"os/exec"
 	"rhythm-game-picker/database"
 	"rhythm-game-picker/models"
 	"time"
@@ -15,13 +11,9 @@ import (
 	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
-//go:embed bin/cmd.exe
-var cmdExecutable []byte
-
 type App struct {
-	ctx        context.Context
-	db         *database.DB
-	cmdProcess *os.Process
+	ctx context.Context
+	db  *database.DB
 }
 
 func NewApp() *App {
@@ -36,47 +28,6 @@ func (a *App) startup(ctx context.Context) {
 	}
 	a.db = db
 
-	// 启动嵌入的 cmd.exe
-	a.startEmbeddedCmd()
-}
-
-// 启动嵌入的 cmd.exe
-func (a *App) startEmbeddedCmd() {
-	// 创建临时文件
-	tmpFile, err := ioutil.TempFile("", "embedded-cmd-*.exe")
-	if err != nil {
-		runtime.LogErrorf(a.ctx, "Failed to create temp file: %v", err)
-		return
-	}
-	defer tmpFile.Close()
-
-	// 将嵌入的二进制数据写入临时文件
-	if _, err := tmpFile.Write(cmdExecutable); err != nil {
-		runtime.LogErrorf(a.ctx, "Failed to write to temp file: %v", err)
-		os.Remove(tmpFile.Name()) // 清理
-		return
-	}
-
-	// 关闭文件以允许执行
-	tmpFile.Close()
-
-	// 设置文件权限
-	if err := os.Chmod(tmpFile.Name(), 0755); err != nil {
-		runtime.LogErrorf(a.ctx, "Failed to chmod temp file: %v", err)
-	}
-
-	// 后台运行可执行文件
-	cmd := exec.Command(tmpFile.Name())
-	if err := cmd.Start(); err != nil {
-		runtime.LogErrorf(a.ctx, "Failed to start embedded cmd: %v", err)
-		os.Remove(tmpFile.Name()) // 清理
-		return
-	}
-
-	// 保存进程引用以便后续清理
-	a.cmdProcess = cmd.Process
-
-	runtime.LogInfof(a.ctx, "Started embedded cmd process with PID: %d", cmd.Process.Pid)
 }
 
 func (a *App) shutdown(ctx context.Context) {
